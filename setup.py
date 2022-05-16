7d7bdc246ad9b89eb78c0058417ed8b3bde57d4e
pwmax <hqwhq0@gmail.com>
2022-05-16 21:14:55 +0700

    fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'eztorch',
     version = '0.1.0',
     description = 'Train pytorch models in one line',
     license='MIT',
-    packages = ['eztorch'],
+    packages = find_packages(
+        where='eztorch'
+    ),
     python_requires='>=3.7',
     include_package_data=True
 )
